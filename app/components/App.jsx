b6f674e3ea1c209b9b202056d5451b8aa1132f49
Ling Tran <lxiutran@gmail.com>
2017-09-06 16:07:56 -0500

    Introduce Application component file - Still Hello World

---
+++ app/components/App.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <div className="Greeting">
+        <p> Hello World </p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('application'))
--- app/index.js
+++ app/index.js
@@ -1,16 +1,2 @@
 require('./main')
-
-const React = require('react')
-const ReactDOM = require('react-dom')
-
-class HelloWorld extends React.Component {
-  render () {
-    return (
-      <div className="Greeting">
-        <p> Hello World </p>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<HelloWorld />, document.getElementById('application'))
+require('./components/App')
