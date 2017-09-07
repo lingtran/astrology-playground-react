e15324539dac6ac3343880fe5bf8cb1cfbf97e38
Ling Tran <lxiutran@gmail.com>
2017-09-07 14:17:15 -0500

    Add calendar picker to ZodiacForm component

--- app/components/ZodiacForm.jsx
+++ app/components/ZodiacForm.jsx
@@ -1,17 +1,45 @@
 const React = require('react')
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import { Button, Form, FormGroup, Label } from 'reactstrap'
+import { moment } from 'moment'
+import DayPickerInput from 'react-day-picker/DayPickerInput'
+import "react-day-picker/lib/style.css"
 
 class ZodiacForm extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      selectedDay: undefined
+    }
+  }
+
+ handleDayChange (selectedDay) {
+    this.setState({ selectedDay: selectedDay });
+  };
+
   render () {
+    const datePickerProps = {
+      enableOutsideDays: true,
+    };
+
+    const value = this.state.selectedDay ? this.state.selectedDay.format('DD/MM/YYYY') : '';
+
     return (
-      <Form inline>
-        <FormGroup>
-          <Label for="test" hidden>Test</Label>
-          <Input type="test" name="test" id="testInput" placeholder="Test" />
-        </FormGroup>
-        {' '}
-        <Button>Submit</Button>
-      </Form>
+      <div className="DayPicker">
+        <Label for="input">Please enter your birthday</Label>
+        <Form inline>
+          <FormGroup>
+            <DayPickerInput
+              name="birthday"
+              placeholder = "DD/MM/YYYY"
+              format="DD/MM/YYYY"
+              value={value}
+              onDayChange={this.handleDayChange}
+            />
+          </FormGroup>
+          {' '}
+          <Button>Submit</Button>
+        </Form>
+      </div>
     )
   }
 }
